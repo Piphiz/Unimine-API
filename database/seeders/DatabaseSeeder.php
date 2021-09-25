f93df22d25ab01a4811797e47baeed680abac9a8
Eduardo Berg <77705253+Piphiz@users.noreply.github.com>
2021-09-25 00:05:51 -0300

    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\UrlActivity;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        $this->call(UrlSeeder::class);
+        $this->call(UrlEntriesSeeder::class);
+        $this->call(UrlActivitiesSeeder::class);
+        \App\Models\User::factory(1)->create();
     }
 }
