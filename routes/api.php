0c781d7cce17339a5b3a429141aad45b11750093
Eduardo Berg <77705253+Piphiz@users.noreply.github.com>
2021-09-27 09:22:59 -0300

    resource to apiResource

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('url', UrlController::class);
+Route::apiResource('url', UrlController::class);
