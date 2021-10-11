7bffbd44668d0a4b24331b385e1f870dab9ce712
Eduardo Berg <77705253+Piphiz@users.noreply.github.com>
2021-10-11 20:50:00 -0300

    Revert "Correcao de Url deletadas"
    This reverts commit 787498d798baae62e3acdcd763edc1d3e55990b1.

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -53,9 +53,6 @@ class UrlController extends Controller
             $url = $this->url->findByLink($data['link']);
 
             if ($url) {
-                if(!is_null($url->deleted_at)){
-                    $url->restore();
-                }
                 return response()->json([
                     'data' => [
                         'id' => $url->id,
