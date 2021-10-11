87b0aacb6104c4ab6ed5a250045127ce6fef8a15
Julio Alves <85263694+Julioalvbarreto@users.noreply.github.com>
2021-10-11 10:01:21 -0300

    adicionado status code no middleware ApiVerify

--- app/Http/Middleware/ApiVerifyAuth.php
+++ app/Http/Middleware/ApiVerifyAuth.php
@@ -25,11 +25,11 @@ class ApiVerifyAuth extends BaseMiddleware
             $user = JWTAuth::parseToken()->authenticate();
         } catch (Exception $exception) {
             if ($exception instanceof TokenInvalidException) {
-                return response()->json(['status' => 'Token is Invalid']);
+                return response()->json(['status' => 'Token is Invalid'], 401);
             } else if ($exception instanceof TokenExpiredException) {
-                return response()->json(['status' => 'Token is Expired']);
+                return response()->json(['status' => 'Token is Expired'], 401);
             } else {
-                return response()->json(['status' => 'Authorization Token not found']);
+                return response()->json(['status' => 'Authorization Token not found'], 401);
             }
         }
 
